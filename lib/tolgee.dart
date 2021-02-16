702548f8f209068fc690f37c3edced7aa0812159
Jan Cizmar <cizmar@chlupac.com>
2021-02-16 19:12:12 +0100

    first commit

---
+++ lib/tolgee.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library tolgee;
+
+export 'src/tolgee_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
